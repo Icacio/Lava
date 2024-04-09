05c806184215eada697f59a2f19fafd8f2373d52
Isaac <isaacjugo@gmail.com>
2024-04-09 16:54:11 -0600

    migrations correction

--- resources/views/components/toolBar.blade.php
+++ resources/views/components/toolBar.blade.php
@@ -10,9 +10,10 @@
 		<a href="/new"><b>Crear</b> un nuevo Post</a>
 	@endunless
 	@if(request()->is('post/*'))
-		@if($postId->user_id==auth()->user()->id)
-		<a href="/edit/$id">Edit post</a>	
-		@endif
+		{{request()->path()}}
+		
+		<a href="/edit/*">Edit post</a>	
+		
 	@endif
 </p>
 @endauth
